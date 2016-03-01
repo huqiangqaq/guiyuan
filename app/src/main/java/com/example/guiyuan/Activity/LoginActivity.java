ae0dab53ebfad8ad3219b2fe443621adf870a5ce
huqiang <839168655@qq.com>
2016-03-01 23:26:59 +0800

    测试

--- app/src/main/java/com/example/guiyuan/Activity/LoginActivity.java
+++ app/src/main/java/com/example/guiyuan/Activity/LoginActivity.java
@@ -65,6 +65,7 @@ public class LoginActivity extends BaseActivity {
         /**
          * 测试2253397848
          *121212121212
+         * 2253397848
          */
         if (!NetCheckUtil.isConnected(this)){
             Toast.makeText(this,"当前无网络连接，请检查网络设置",Toast.LENGTH_SHORT).show();
